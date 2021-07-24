079ef272b831da2ea91e113985952d180ec6e7de
dgleyramos1 <dgleysilva@gmail.com>
2021-07-24 14:34:58 -0300

    Modificando a baseURL da api fake

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8888/produtos'
+  baseURL: 'https://my-json-server.typicode.com/dgleyramos1/demo/produtos'
 })
 
 export default api;
